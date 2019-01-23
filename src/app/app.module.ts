979ff428481a54ace2f9900fc7385e238cdb08dd
konstantin <k.schraubek@web.de>
2019-01-23 08:56:46 +0100

    added detail view component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { EncryptComponent } from './components/encrypt/encrypt.component';
+import { DetailViewComponent } from './components/detail-view/detail-view.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    EncryptComponent
+    EncryptComponent,
+    DetailViewComponent
   ],
   imports: [
     BrowserModule,
