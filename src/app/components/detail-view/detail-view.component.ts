72e30c0006a2b4f668bc0c837ba98e20eb72bd0c
konstantin <k.schraubek@web.de>
2019-01-24 14:36:26 +0100

    more details in view

--- src/app/components/detail-view/detail-view.component.ts
+++ src/app/components/detail-view/detail-view.component.ts
@@ -17,4 +17,41 @@ export class DetailViewComponent implements OnInit {
 
   }
 
+  get selected(): Rezept {
+    return this.recipeService.recipes[0];
+  }
+
+  get ingridients1(): string[] {
+    let test: string[] = [];
+    for(let i = 0; i < this.selected.ingredientLines.length / 2; i++){
+      test.push(this.selected.ingredientLines[i]);
+    }
+    return test;
+  }
+
+  get ingridients2(): string[] {
+    let test: string[] = [];
+    for(let i = this.selected.ingredientLines.length / 2; i < this.selected.ingredientLines.length; i++){
+      test.push(this.selected.ingredientLines[i]);
+    }
+    return test;
+  }
+
+  get time(): number {
+    // @ts-ignore
+    return this.selected.totalTime.toFixed(2);
+  }
+
+  get weight(): number {
+    // @ts-ignore
+    return this.selected.totalWeight.toFixed(2);
+  }
+
+  get calories(): number {
+    // @ts-ignore
+    return this.selected.calories.toFixed(2);
+  }
+
+
+
 }
