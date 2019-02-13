4d21033f23fae0a275b9a84b7f34a41ffe9cae05
Jendrik <jendrik.dathe@gmail.com>
2019-02-13 10:14:28 +0100

    #130

--- src/app/components/startseite/startseite.component.ts
+++ src/app/components/startseite/startseite.component.ts
@@ -6,10 +6,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./startseite.component.css']
 })
 export class StartseiteComponent implements OnInit {
-
   constructor() { }
 
   ngOnInit() {
   }
 
+  startSearch(ingredients: string) {
+    let locationString = 'list?ingredients=';
+    ingredients.split(',').forEach(ingredient => locationString += ingredient + '+');
+    locationString = locationString.slice(0, -1);
+    window.location.href = locationString;
+  }
 }
