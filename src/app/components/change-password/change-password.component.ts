92e29945d9ad1fc97cb99e43a82de15c1534d2f2
konstantin <k.schraubek@web.de>
2019-02-14 14:03:32 +0000

    added usersettings added layout of change password minor changes in change email

--- src/app/components/change-password/change-password.component.ts
+++ src/app/components/change-password/change-password.component.ts
@@ -9,7 +9,20 @@ export class ChangePasswordComponent implements OnInit {
 
   constructor() { }
 
+  public oldPassword: string;
+  public newPassword: string;
+  public repeatNewPassword: string;
+
+  public error: string;
+
   ngOnInit() {
   }
 
+  changePassword(): void{
+    console.log(this.oldPassword + " " + this.newPassword + " " + this.repeatNewPassword);
+    this.newPassword = "";
+    this.oldPassword = "";
+    this.repeatNewPassword = "";
+    this.error = "testError";
+  }
 }
