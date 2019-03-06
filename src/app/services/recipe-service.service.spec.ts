73c6300ce7142b7086a2e9bac1c8782961711866
dominik499 <dominik499@schule.bremen.de>
2019-03-06 08:38:58 +0000

    Tests hinzugefügt und fehlgeschlagene Test behoben

--- src/app/services/recipe-service.service.spec.ts
+++ src/app/services/recipe-service.service.spec.ts
@@ -2,6 +2,7 @@ import { TestBed } from '@angular/core/testing';
 
 import { RecipeServiceService } from './recipe-service.service';
 import { HttpClientModule} from '@angular/common/http';
+import {Rezept} from '../models/rezept';
 
 describe('RecipeServiceService', () => {
   beforeEach(() => TestBed.configureTestingModule({
@@ -11,5 +12,11 @@ describe('RecipeServiceService', () => {
   it('should be created', () => {
     const service: RecipeServiceService = TestBed.get(RecipeServiceService);
     expect(service).toBeTruthy();
+  });
+
+
+  it('should change selected recipe', () => {
+    const service: RecipeServiceService = TestBed.get(RecipeServiceService);
+    expect(service.selected).toBeUndefined();
   });
 });
